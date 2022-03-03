8c56a194d7949c693b6575807fcb7c74f643d26f
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-03-03 15:49:18 +0100

    Add spec for versions

--- spec/lib/decidim/phone_authorization_handler_spec.rb
+++ spec/lib/decidim/phone_authorization_handler_spec.rb
@@ -8,4 +8,10 @@ describe Decidim::PhoneAuthorizationHandler do
       expect(described_class.version).to eq("1.0.0")
     end
   end
+
+  describe "#decidim_compatibility_version" do
+    it "returns decidim compatibility version" do
+      expect(described_class.decidim_compatibility_version).to eq("0.25.2")
+    end
+  end
 end
