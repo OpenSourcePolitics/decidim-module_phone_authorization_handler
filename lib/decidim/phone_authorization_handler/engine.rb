3fa308b426be84ebc3b9f9b139511115b0dddaa3
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-07-26 10:38:11 +0200

    Remove exporters extends

--- lib/decidim/phone_authorization_handler/engine.rb
+++ lib/decidim/phone_authorization_handler/engine.rb
@@ -8,22 +8,6 @@ module Decidim
     # This is the engine that runs on the public interface of phone_authorization_handler.
     class Engine < ::Rails::Engine
       isolate_namespace Decidim::PhoneAuthorizationHandler
-
-      config.to_prepare do
-        Decidim::Proposals::ProposalSerializer.prepend(Decidim::PhoneAuthorizationHandler::Extends::ProposalSerializerExtend)
-
-        Decidim::Exporters::CSV.prepend(Decidim::PhoneAuthorizationHandler::Extends::CSVExporterExtend)
-        Decidim::Exporters::Excel.prepend(Decidim::PhoneAuthorizationHandler::Extends::ExcelExporterExtend)
-        Decidim::Exporters::JSON.prepend(Decidim::PhoneAuthorizationHandler::Extends::JSONExporterExtend)
-      end
-
-      initializer "decidim.phone_authorization_handler" do
-        require "decidim/exporters/serializer"
-        require "decidim/exporters/csv"
-        require "decidim/exporters/json"
-        require "decidim/exporters/excel"
-        require "decidim/proposals/proposal_serializer"
-      end
     end
   end
 end
